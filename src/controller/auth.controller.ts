55f5c46a99603832cba2b13054d287eddd0d518f
Ali Saleh <ali_saleh-4@hotmail.fr>
2023-05-16 15:36:37 +0200

    feat : /me verify token

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from "express";
-import { findUserPerEmail } from "../queries/user.queries";
-import { createJwtToken } from "../utils/jwt.utils"
-
-
+import { findUserPerEmail,createUser } from "../queries/user.queries";
+import { createJwtToken,extractUserFromToken } from "../utils/jwt.utils"
+import { userSignupValidation } from "../database/validation/user.validation";
+import  { ValidationError } from "joi";
 
 export const login = async (req: Request, res: Response, _: NextFunction) => {
 
@@ -19,30 +19,66 @@ export const login = async (req: Request, res: Response, _: NextFunction) => {
                     token: token,
                 });
             } else {
-                res.status(404).json({ message: "Mauvais identifiants" });
+                res.status(404).json({ error: "Mauvais identifiants" });
             }
         } else {
-            res.status(404).json({ message: "Mauvais identifiants" });
+            res.status(404).json({ error: "Mauvais identifiants" });
         }
     } catch (e) {
-        res.status(404).json({ message: "Error" });
+        res.status(404).json({ error: "Error" });
     }
 
 };
 
-export const me = async (_: Request, res: Response) => {
+export const me = async (req: Request, res: Response) => {
+    
+    if (req.headers.authorization  != undefined) {
+         res.status(401).json({ error: "Not Authorized" });
+      }
 
-    res.status(404).json("error");
+      const authHeader = req.headers.authorization as string;
+      const token = authHeader.split(" ")[1];
+    
+      try {
+        // Verify the token is valid
+        const  user  = extractUserFromToken(token)
+        if (user){
+           res.status(200).json(user);
+        }else{
+            res.status(404).json({ error: "Error" });
+        }
+
+      } catch (error) {
+        res.status(404).json({ error: "Error" });
+      }
 
 };
 
 
 
 
-export const signup = async (__: Request, res: Response, _: NextFunction) => {
+export const signup = async (req: Request, res: Response, _: NextFunction) => {
+    try {
+        await userSignupValidation.validateAsync(req.body, { abortEarly: false });
+    
+        const body = req.body;
 
-
-    res.status(404).json("error");
+        const user = await createUser(body);
+        const token = createJwtToken({ user, id: undefined })
+        res.status(200).json({
+            token: token,
+        });
+        } catch (e) {
+        const errors = [];
+        if (e instanceof ValidationError) {
+          e.details.map((error) => {
+            errors.push({ field: error.path[0], message: error.message });
+          });
+        } else {
+            errors.push({ field: "error", message: e })
+        }
+        res.status(404).send(errors);
+      }
 
 };
 
--- src/queries/user.queries.ts
+++ src/queries/user.queries.ts
@@ -1,5 +1,5 @@
 import {User} from "../database/models/user.model";
-import { IUser } from "../interfaces";
+import { IUser, UserForm } from "../interfaces";
 
 
 export const findUserPerEmail = (email:string):Promise<IUser| null> => {
@@ -9,3 +9,18 @@ export const findUserPerEmail = (email:string):Promise<IUser| null> => {
 export const findUserPerId = (id:string):Promise<IUser| null>  => {
   return User.findById(id).populate("role").populate("image").exec();
 };
+
+
+export const createUser= async (user :UserForm):Promise<IUser| null> => {
+      const hashedPassword =  await User.hashPassword(user.password);
+      const newUser  = new User({
+        username: user.username,
+        firstname:user.firstname,
+        lastname:user.lastname,
+        local: {
+          email: user.email,
+          password: hashedPassword,
+        },
+      });
+      return await newUser.save();
+  };
